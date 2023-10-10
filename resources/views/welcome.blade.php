e4d051f2129bb922307107cc910cdac8895c6d18
jbolozdina <jbolozdina@gmail.com>
2023-10-10 22:15:03 +0300

    Removed message count

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -811,9 +811,6 @@
             </svg>
         </div>
 
-        <div class="text-center text-gray-500 dark:text-gray-400 mt-16">
-            Current message count: 0
-        </div>
         <div class="text-center text-gray-500 dark:text-gray-400 mt-8">
             Woah.
         </div>
